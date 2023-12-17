fec6f54228b2624001ef160a24dde09e59360bf8
raacee <raceldamechli@outlook.fr>
2023-12-17 18:11:53 +0100

    restauré les routes de seb

--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
 import express from 'express';
-import { client } from 'db'
+import {client} from "./db";
 
-const app = express();
-const port = 3000;
+const port = 3000
+const app = express()
 
 app.use(express.json());
 
 app.get('/', function(req,res){
-
+  res.sendStatus(200)
 })
 
-app.get('/api/learningPackages', async (req, res) => {
-  await client.connect()
+app.get('/api/learningPackages', function(req, res) {
+  client.connect((err, client, done) => {
+    if (err) {
+      console.error('Error acquiring client from pool:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    client.query('SELECT * FROM LearningPackages', (err, result) => {
+      done(); // Release the client back to the pool
+
+      if (err) {
+        console.error('Error retrieving learning packages:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      const learningPackages = result.rows;
+      res.json(learningPackages);
+    });
+  });
 })
 
 app.listen(port, () => {
