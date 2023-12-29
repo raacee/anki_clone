975cac209c8a2affed7abac42fbaa501cda427e4
raacee <raceldamechli@outlook.fr>
2023-12-29 14:19:14 +0100

    Ajout de routes pour index.html

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getAllLearningPackages } = require("./db.js");
 
-const PORT = 3000
+const PORT = 4000
 const app = express()
 
 app.use(express.json());
@@ -16,23 +16,48 @@ app.get('/home', function(req, res){
 app.get('/non-study-packages', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/index.html')
 })
-
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('lesson-list/:id', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('lesson/:id', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('test-page1', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('non-study-packages', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('achievements-page', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('package-creation-page', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('learning-facts-page/:id', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('modify-learning-fact-page/:packageId/:factId', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
+app.get('add-learning-fact-page/:packageId', function(req,res){
+	res.sendFile(__dirname + '/angular/dist/index.html')
+})
 app.get('/main.js', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/main.js')
 })
-
 app.get('/vendor.js', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/vendor.js')
 })
-
 app.get('/polyfills.js', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/polyfills.js')
 })
-
 app.get('/runtime.js', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/runtime.js')
 })
-
 app.get('/styles.css', function(req, res){
 	res.sendFile(__dirname + '/angular/dist/styles.css')
 })
