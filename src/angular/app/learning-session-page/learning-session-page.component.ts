3b74507b04b1d62abc2ff183d1bdd7b7d4dbe4be
sebmoine <seb.moine@yahoo.fr>
2023-12-29 21:30:43 +0100

    Update learning-session-page.component.ts

--- src/angular/app/learning-session-page/learning-session-page.component.ts
+++ src/angular/app/learning-session-page/learning-session-page.component.ts
@@ -46,23 +46,49 @@ export class LearningSessionPageComponent {
   }
 
 /*
-  async updateFact(packageId: number, factId: number, confidenceLevel: number): Promise<void> {
-    try {
-      const response = await fetch(`/api/learningpackages/${packageId}/facts/${factId}`, {
-        method: 'PUT', // or 'POST', depending on your backend setup
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ confidenceLevel })
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      // Optionally, process the response if needed
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+async updateFact(packageId: number, factId: number, confidenceLevel: number): Promise<void> {
+  // Update local data first
+  const pkg = this.learningPackages.find(p => p.id === packageId);
+  const fact = pkg?.questions.find(f => f.id === factId);
+  if (fact) {
+    fact.reviewCount += 1;
+    fact.lastReviewedDate = new Date(); // Today
+    fact.confidenceLevel = confidenceLevel;
+    switch (confidenceLevel) {
+      case 1:
+        fact.nextDate = new Date(fact.lastReviewedDate.getTime() + 7 * 24 * 60 * 60 * 1000); // +1 week
+        break;
+      case 2:
+        fact.nextDate = new Date(fact.lastReviewedDate.getTime() + 4 * 24 * 60 * 60 * 1000); // +4 days
+        break;
+      case 3:
+        fact.nextDate = new Date(fact.lastReviewedDate.getTime() + 1 * 24 * 60 * 60 * 1000); // +1 day
+        break;
+      case 4:
+        fact.nextDate = new Date(fact.lastReviewedDate.getTime()); // today
+        break;
     }
   }
+
+  // Now, send an update request to the backend
+  try {
+    const updatedFactData = { ...fact }; // Create a copy of the fact to send
+    const response = await fetch(`/api/learningpackages/${packageId}/facts/${factId}`, {
+      method: 'PUT', // or 'POST', depending on your backend setup
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedFactData)
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Optionally, process the response if needed
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
   */
 
 
