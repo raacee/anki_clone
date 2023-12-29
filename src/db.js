934fa84531e92466e9b1a05a2f910b4509ed21a5
raacee <raceldamechli@outlook.fr>
2023-12-29 14:19:39 +0100

    récupération de tout les LF et ULP sous le bon format FONCTIONNE

--- src/db.js
+++ src/db.js
@@ -1,22 +1,51 @@
-const { LearningPackage } = require('./models.js')
+const { UserLearningPackage, LearningFact } = require('./models.js')
 
-async function getAllLearningPackages(req,res){
-    await LearningPackage.findAll()
-        .then((learningPackages) => {
-            // Process retrieved learning packages
-            res.json(learningPackages);
-        })
-        .catch((err) => {
-            console.error('Error retrieving learning packages:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        });
+async function fetchLearningPackages(){
+    return await UserLearningPackage.findAll()
 }
 
-async function getUserLearningPackages(){
-
+async function getLearningFactsByULP(ulp_id){
+    return await LearningFact.findAll(
+        {
+            where:{
+                ULP_id:ulp_id
+            }
+        }
+    )
 }
+
+async function getAllLearningPackages(){
+    const res = []
+    const ulps = await fetchLearningPackages()
+    for(const ulp of ulps){
+        let ULP = {
+                id:ulp.ULP_id,
+                category: ulp.ULP_category,
+                description: ulp.ULP_description,
+                title: ulp.ULP_title,
+                difficultyLevel: ulp.ULP_difficultyLevel,
+                isAchieved : ulp.ULP_isAchieved,
+                isStudyProgram  : ulp.ULP_isStudyProgram
+            }
+        const lfs = await getLearningFactsByULP(ulp.ULP_id)
+        const questions = []
+        for(const lf of lfs){
+            questions.push({
+                id:lf.LF_ID,
+                question:lf.LF_question,
+                answer:lf.LF_answer
+            })
+        }
+        ULP.questions = questions
+        res.push(ULP)
+    }
+    return res
+}
+
+getAllLearningPackages().then((res)=>{
+    console.log(res)
+})
 
 module.exports = {
-    getAllLearningPackages,
-    getUserLearningPackages
+    getAllLearningPackages
 }
