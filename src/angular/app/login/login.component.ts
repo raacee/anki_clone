5ff19184f535a785ad257ac4af18a51041273ab3
raacee <raceldamechli@outlook.fr>
2023-12-31 00:17:49 +0100

    codé le register & login / pas testé

--- src/angular/app/login/login.component.ts
+++ src/angular/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import {FormGroup, FormControl, ɵValue} from "@angular/forms";
+import {Event, Router, } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  wrongPassword = false
+  loginForm = new FormGroup({
+    username: new FormControl(""),
+    password: new FormControl("")
+  });
   constructor(private router: Router) {}
 
-  username: string = '';
-  password: string = ''; // Make sure you handle the password securely
-
-  login() {
-    localStorage.setItem('username', this.username);
-    this.router.navigate(['/home-page']); // Replace with your actual route
+  async login(event:any) {
+    /*localStorage.setItem('username', this.username);
+    this.router.navigate(['/home-page']); // Replace with your actual route*/
+    const username = this.loginForm.value.username
+    const password = this.loginForm.value.password
+    const res = await fetch('/api/login', {
+      method:'POST',
+      headers:{'Content-Type': 'application/json'},
+      body:JSON.stringify({username:username,password:password})
+    })
+    if (res.ok){
+      const { session_id } = (await res.json())
+      localStorage.setItem('session_id', session_id)
+    }
+    else{
+      this.wrongPassword = true
+    }
+    event.stopPropagation();
+  }
+  register(){
+    this.router.navigate(['/register'])
   }
 }
--- src/angular/app/register/register.component.ts
+++ src/angular/app/register/register.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
+    signupForm = new FormGroup({
+        username: new FormControl(""),
+        password: new FormControl("")
+    });
+    isNotAvailable = false
 
-  constructor(private router: Router) {}
+    constructor(private router: Router) {
+    }
 
-  register() {
-    // Implement registration logic here
-    // For now, we'll just log the input and navigate to the login page
-    console.log('Register with:', this.username, this.email, this.password);
-    
-    // TODO: Implement actual registration logic
-    
-    // Navigate to the login page after registration
-    this.router.navigate(['/login']);
-  }
+    login() {
+        this.router.navigate(['/login'])
+    }
+
+    async signup(event: any) {
+        event.stopPropagation()
+        const username = this.signupForm.value.username
+        const password = this.signupForm.value.password
+        const res = await fetch('/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password})
+        })
+        if (!res.ok) {
+            this.isNotAvailable = true
+        }
+        else {
+            this.login()
+        }
+    }
 }
