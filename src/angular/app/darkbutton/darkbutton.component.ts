3497cb1714d95f422142b9fede9b88c6f6b4888f
HayalAhl <133215818+HayalAhl@users.noreply.github.com>
2023-12-30 16:38:54 +0100

    made dark button

--- src/angular/app/darkbutton/darkbutton.component.ts
+++ src/angular/app/darkbutton/darkbutton.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { enable as enableDarkMode, disable as disableDarkMode } from 'darkreader';
 
 @Component({
   selector: 'app-darkbutton',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./darkbutton.component.css']
 })
 export class DarkbuttonComponent {
+  active:boolean
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.active = !!localStorage.getItem('dark')
+  }
+
+  isDark(){
+    return this.active
+  }
+
+  isLight(){
+    return !this.active
+  }
+
+  modeToggle() {
+    if (this.active || this.document.querySelector('body')?.classList.contains('dark-mode')) {
+      this.light()
+    }
+    else {
+      this.dark()
+    }
+    this.active = !this.active
+  }
+
+  dark() {
+    this.document.querySelector('body')?.classList.add('dark-mode')
+    enableDarkMode({
+      brightness: 100,
+      contrast: 100,
+      sepia: 0
+    })
+    localStorage.setItem('dark','true')
+  }
+
+  light() {
+    this.document.querySelector('body')?.classList.remove('dark-mode')
+    disableDarkMode()
+    localStorage.setItem('dark','false')
+  }
 
 }
