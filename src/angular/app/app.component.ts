0ae92b507a7df6522d9351641e05c87a80b27c3d
raacee <raceldamechli@outlook.fr>
2023-12-31 00:15:10 +0100

    supp des console.log

--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -9,12 +9,12 @@ import { MenuNavBarComponent } from './menu-nav-bar/menu-nav-bar.component';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'ankiclone';
   constructor() {
-    console.log('AppComponent.constructor()');
+
   }
   ngOnInit(): void {
-    console.log('AppComponent.ngOnInit()');
+
   }
   ngOnDestroy() {
-    console.log('AppComponent.ngOnDestroy()')
+
   }
 }
